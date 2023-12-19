fd7d75fed52ebf657ef31ae632fa97cb03851f2f
Markus Zoppelt <markus.zoppelt@code-intelligence.com>
2023-12-19 16:12:14 +0100

    test(container): test container remote-run for monitor interval

--- e2e/tests/container/remoterun_test.go
+++ e2e/tests/container/remoterun_test.go
@@ -1,7 +1,11 @@
 package container_test
 
 import (
+	"io"
+	"net/http"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 
 	"code-intelligence.com/cifuzz/e2e"
 	"code-intelligence.com/cifuzz/integration-tests/shared/mockserver"
@@ -50,6 +54,24 @@ var containerRemoteRunTests = &[]e2e.TestCase{
 				ErrorContains("Finding found: test_finding, NID: fdn-testtesttesttest")
 		},
 	},
+	{
+		Description:   "container remote-run command in a maven/gradle example directory with monitor mode and check for duration limit.",
+		CIUser:        e2e.LoggedInCIUser,
+		Command:       "container remote-run",
+		Args:          []string{" --project test-project --registry localhost:5000/test/cifuzz com.example.FuzzTestCase::myFuzzTest --monitor --monitor-duration 10s --monitor-interval 5s -v"},
+		SampleFolder:  []string{"../../../examples/maven"},
+		ToolsRequired: []string{"docker", "java", "maven"},
+		SkipOnOS:      "windows",
+		Assert: func(t *testing.T, output e2e.CommandOutput) {
+			output.Success().
+				ErrorContains("Created fuzz container image with ID ").
+				ErrorContains("Start uploading image ").
+				ErrorContains("to localhost:5000/test/cifuzz").
+				ErrorContains("The push refers to repository [localhost:5000/test/cifuzz]").
+				ErrorContains("Max monitor duration is 10 seconds.").
+				ErrorNotContains("Finding found: test_finding, NID: fdn-testtesttesttest")
+		},
+	},
 }
 
 func TestContainerRemoteRun(t *testing.T) {
@@ -57,6 +79,19 @@ func TestContainerRemoteRun(t *testing.T) {
 	mockServer.Handlers["/v1/projects"] = mockserver.ReturnResponse(t, mockserver.ProjectsJSON)
 	mockServer.Handlers["/v3/runs"] = mockserver.ReturnResponse(t, mockserver.ContainerRemoteRunResponse)
 	mockServer.Handlers["/v3/runs/run-testtesttesttest/status"] = mockserver.ReturnResponse(t, mockserver.ContainerRemoteRunStatusResponse)
-	mockServer.Handlers["/v3/runs/run-testtesttesttest/findings"] = mockserver.ReturnResponse(t, mockserver.ContainerRemoteRunFindingsResponse)
+
+	requestCount := 0
+	mockServer.Handlers["/v3/runs/run-testtesttesttest/findings"] = func(w http.ResponseWriter, req *http.Request) {
+		if requestCount >= 5 {
+			_, err := io.WriteString(w, mockserver.ContainerRemoteRunFindingsResponse)
+			require.NoError(t, err)
+			requestCount = 0
+		} else {
+			_, err := io.WriteString(w, mockserver.ContainerRemoteRunNoFindingsResponse)
+			require.NoError(t, err)
+			requestCount++
+		}
+	}
+
 	e2e.RunTests(t, *containerRemoteRunTests, mockServer)
 }
