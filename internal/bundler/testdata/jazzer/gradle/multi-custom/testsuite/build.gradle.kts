0d097e845c7ed0a969e23e4fb87b3c332423e0d8
mlsandnerCI <maren.sandner@code-intelligence.com>
2024-01-16 13:18:41 +0100

    feat(gradle): credential instructions works for all versions
    ... and all settings.gradle/build.gradle files have been updated with the new approach

--- internal/bundler/testdata/jazzer/gradle/multi-custom/testsuite/build.gradle.kts
+++ internal/bundler/testdata/jazzer/gradle/multi-custom/testsuite/build.gradle.kts
@@ -7,7 +7,10 @@ repositories {
 	maven {
 		name = "CodeIntelligenceRepository"
 		url = uri("https://gitlab.code-intelligence.com/api/v4/projects/89/packages/maven")
-		credentials(PasswordCredentials::class)
+		credentials {
+			username = extra["CodeIntelligenceRepositoryUsername"].toString()
+			password = extra["CodeIntelligenceRepositoryPassword"].toString()
+		}
 		content {
 			includeGroupByRegex("com\\.code-intelligence.*")
 		}
